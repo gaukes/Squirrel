0a99cc937cb2b5eee559bf776db6f7e93287171e
Nicholas Xu <nick.xu.47@gmail.com>
2015-10-10 21:54:12 -0700

    basic template displaying capabilities

--- squirrel.js
+++ squirrel.js
@@ -1,1 +1,14 @@
 Purchases = new Meteor.Collection( "Purchases" );
+if (Meteor.isClient) {
+    Session.set("activeelement", "purchases")
+
+    Template.body.helpers({
+        "active": function(element) {
+            console.log(element)
+            if (Session.get("activeelement") == element) {
+                return true
+            }
+            return false
+        },
+    });
+}
